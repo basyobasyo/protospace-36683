0ddb87c226fb655d7d5c4d41e4ce412122235245
basyobasyo <om1997518@gmail.com>
2021-11-13 08:49:12 +0900

    削除機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -17,6 +17,30 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def show
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+      if @prototype.update(prototype_params)
+       # ↑この@を無しにするとrenderでeditアクションに戻った際に、フォームの情報はのこらない。理屈は？
+        redirect_to root_path
+      else
+        render :edit
+      end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
   private
 
   def prototype_params
